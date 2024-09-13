508f08d039a4abaca81dd854cbe96db9921e2d11
crisprborg <davidadongo002@gmail.com>
2024-09-13 10:36:54 +0300

    routes files merged into controllers

--- src/routes/gcpsActivityRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { logActivity } = require('../controllers/activityController');
-const router = express.Router();
-
-router.post('/log', logActivity);
-
-module.exports = router;
--- src/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { depositMoney } = require('../controllers/paymentController');
-const router = express.Router();
-
-router.post('/deposit', depositMoney);
-
-module.exports = router;
