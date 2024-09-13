af842315aa8fd43ddf558ddebe736f762035482a
crisprborg <davidadongo002@gmail.com>
2024-09-13 11:01:45 +0300

    deleted some files for cleaner setup!

--- src/controllers/paymentController.js
+++
@@ -1,9 +0,0 @@
-const { processPayment } = require('../models/payment');
-
-const depositMoney = (req, res) => {
-    const { userId, amount, paymentMethod } = req.body;
-    const paymentResult = processPayment(userId, amount, paymentMethod);
-    res.json(paymentResult);
-};
-
-module.exports = { depositMoney };
