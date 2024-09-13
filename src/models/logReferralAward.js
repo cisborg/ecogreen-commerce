530da5e398b1cb69569d3574055aa1de52591b64
crisprborg <davidadongo002@gmail.com>
2024-09-13 10:43:33 +0300

    deleted some models for clean implementation

--- src/models/logReferralAward.js
+++
@@ -1,4 +0,0 @@
-const logReferralReward = async (userId, points, criteria) => {
-    await pool.query('INSERT INTO awards (user_id, reward_type, points, criteria, description) VALUES ($1, $2, $3, $4, $5)', 
-    [userId, 'referral', points, criteria, 'Reward for successful referrals']);
-};
